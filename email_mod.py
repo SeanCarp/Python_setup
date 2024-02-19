63238211a9d338f1b95e2acc5300d3b19ca5ade6
SeanCarp <swcarpenter04@gmail.com>
2024-02-19 18:38:57 -0500

    test

---
+++ email_mod.py
@@ -0,0 +1,50 @@
+# This library sends a text to the specified phone num
+#
+# It does not have specified email and password, that needs to be
+# created through the google set-up
+
+# This import uses the MIMEText library to format the text
+from email.mime.text import MIMEText
+
+# This import uses smtplib as the connection between the Gmail server
+import smtplib
+
+email = None
+password = None
+server = None
+
+def email_mod(self, email, password):
+    email = self.email
+    password = self.password
+
+    # Setup server connection
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.startls()
+    server.login(email, password)
+    print('Login successful')
+
+
+def send_text(subject, body, *to):
+    if server is not None or email is not None:
+        msg = MIMEText(body)
+        msg['subject'] = subject
+        msg['from'] = email
+
+        if(len(to) == 2):
+            mobile_providers = {'at&t': '@mms.att.net',
+                            'cricket wireless': '@mms.att.net',
+                            'metro pcs': '@metropcs.sms.us',
+                            'tmobile': '@tmomail.net',
+                            'us cellular': '@email.uscc.net',
+                            'verizon': '@vtext.com'}
+            msg['to'] = to[0] + mobile_providers.get(to[1].lower())
+        else:
+            msg['to'] = to[0] # You better make sure that is perfect
+        
+        server.send_message(msg)
+        print('Sent')
+
+
+if __name__ == '__main__':
+    email_mod(self, "swcarpenter04@gmail.com", "qcczprggcwopltpy")
+
