1dd24ad3a8be2640d1de30e664f2cbb2282c0ad7
SeanCarp <swcarpenter04@gmail.com>
2024-02-19 20:49:39 -0500

    update

--- email_mod.py
+++ email_mod.py
@@ -9,43 +9,47 @@ from email.mime.text import MIMEText
 # This import uses smtplib as the connection between the Gmail server
 import smtplib
 
-email = None
-password = None
-server = None
+class Email():
+    email = None
+    password = None
+    server = None
 
-def email_mod(email, password):
-    email = email
-    password = password
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
-    # Setup server connection
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    print('Login successful')
+        # Setup server connection
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+
+        self.server = server
+        print('Login successful')
 
 
-def send_text(subject, body, *to):
-    if server is not None or email is not None:
-        msg = MIMEText(body)
-        msg['subject'] = subject
-        msg['from'] = email
+    def send_text(self, subject, body, *to):
+        print(self.email, self.server)
 
-        if(len(to) == 2):
-            mobile_providers = {'at&t': '@mms.att.net',
-                            'cricket wireless': '@mms.att.net',
-                            'metro pcs': '@metropcs.sms.us',
-                            'tmobile': '@tmomail.net',
-                            'us cellular': '@email.uscc.net',
-                            'verizon': '@vtext.com'}
-            msg['to'] = to[0] + mobile_providers.get(to[1].lower())
-        else:
-            msg['to'] = to[0] # You better make sure that is perfect
-        
-        server.send_message(msg)
-        print('Sent')
+        if self.server is not None or self.email is not None:
+            msg = MIMEText(body)
+            msg['subject'] = subject
+            msg['from'] = self.email
+
+            if(len(to) == 2):
+                mobile_providers = {'at&t': '@mms.att.net',
+                                'cricket wireless': '@mms.att.net',
+                                'metro pcs': '@metropcs.sms.us',
+                                'tmobile': '@tmomail.net',
+                                'us cellular': '@email.uscc.net',
+                                'verizon': '@vtext.com'}
+                msg['to'] = to[0] + mobile_providers.get(to[1].lower())
+            else:
+                msg['to'] = to[0] # You better make sure that is perfect
+            
+            self.server.send_message(msg)
+            print('Sent')
 
 
 if __name__ == '__main__':
-    email_mod("swcarpenter04@gmail.com", "hwreortjnqwtjqcg")
-    send_text("Hello", "I love you", '5407697972', "verizon")
+
 
