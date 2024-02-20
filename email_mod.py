3345ab7280f997e62f3edb09d058943be3c731b5
SeanCarp <swcarpenter04@gmail.com>
2024-02-20 10:56:10 -0500

    final comments

--- email_mod.py
+++ email_mod.py
@@ -1,7 +1,8 @@
-# This library sends a text to the specified phone num
-#
-# It does not have specified email and password, that needs to be
-# created through the google set-up
+""" This library sends a text to the specified phone num
+
+It does not have specified email and password, that needs to be
+created through the google set-up
+"""
 
 # This import uses the MIMEText library to format the text
 from email.mime.text import MIMEText
@@ -10,11 +11,22 @@ from email.mime.text import MIMEText
 import smtplib
 
 class Email():
+    '''A class to hold the data for sending emails'''
+
     email = None
     password = None
     server = None
 
     def __init__(self, email, password):
+        """
+        Initializes the Email class
+
+        Attributes:
+            self (self)
+            email (str): the email used to send out the text
+            password (str): the password used to sign-in to the email
+        """
+
         self.email = email
         self.password = password
 
@@ -28,7 +40,20 @@ class Email():
 
 
     def send_text(self, subject, body, *to):
-        print(self.email, self.server)
+        """
+        Sends the text to the required person
+
+        Atributes:
+            self (self)
+            subject (str): the email of the text
+            body (str): the body of the text
+            *to (str, str): 
+                first one is phone num
+                second one is mobile provider
+
+            to (str):
+                first one is perfect phone email addr
+        """
 
         if self.server is not None or self.email is not None:
             msg = MIMEText(body)
@@ -48,8 +73,3 @@ class Email():
             
             self.server.send_message(msg)
             print('Sent')
-
-
-if __name__ == '__main__':
-
-
